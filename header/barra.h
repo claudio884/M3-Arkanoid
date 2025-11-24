ddafbca06847436b5890b5efabcc3ddbb19175f1
Renan Felipe de Andrade <renanfelipe2210@gmail.com>
2025-11-24 20:08:19 -0300

    Add Barra struct and related function declarations

--- header/barra.h
+++ header/barra.h
@@ -1,1 +1,17 @@
+#ifndef BARRA_H
+#define BARRA_H
+#include "posicao.h"
 
+struct Barra {
+    Posicao posicao;
+    float largura;
+    float altura;
+    float velocidadeX;
+};
+
+void inicializaBarra(Barra* barraX);
+void atualizaBarra(Barra* barraX);
+void desenhaBarra(const Barra* barraX);
+void modificarTamanhoBarra(Barra* barraX, float tamanhoDelta);
+
+#endif
