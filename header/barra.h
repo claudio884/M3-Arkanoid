8f218c1fc778349369ec830ccfbad403442331db
Renan Felipe de Andrade <renanfelipe2210@gmail.com>
2025-11-27 21:18:55 -0300

    Refactor Barra structure and function names

--- header/barra.h
+++ header/barra.h
@@ -1,17 +1,26 @@
 #ifndef BARRA_H
 #define BARRA_H
-#include "posicao.h"
+
+struct Posicao {
+       float x;
+       float y;
+};
 
 struct Barra {
-    Posicao posicao;
+    Posicao posicao; 
     float largura;
     float altura;
     float velocidadeX;
 };
 
-void inicializaBarra(Barra* barraX);
-void atualizaBarra(Barra* barraX);
-void desenhaBarra(const Barra* barraX);
-void modificarTamanhoBarra(Barra* barraX, float tamanhoDelta);
+void inicializarBarra(Barra *barra);
+
+void atualizarBarra(Barra *barra, float dt);
+
+void desenharBarra(Barra *barra);
+
+void aumentarTamanhoBarra(Barra *barra, float tamanhoAdd);
+
+void diminuirTamanhoBarra(Barra *barra, float tamanhoSub);
 
 #endif
