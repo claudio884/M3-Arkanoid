3c52b4d5881908ddfeb19c26f07489ad5d4dd22d
Renan Felipe de Andrade <renanfelipe2210@gmail.com>
2025-11-27 21:43:59 -0300

    Refactor menu functions to use raylib and improve input
    Refactor menu drawing functions to use raylib for rendering and update player name handling.

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,104 +1,182 @@
-#include <string.h>
-#include <stdio.h>
 #include "../header/menu.h"
-#include "../header/estadoJogo.h"
-#include "../header/constante.h"
+#include "../header/constantes.h"
+#include "raylib.h"
+#include <cstring>
 
 void inicializarMenu(Menu *menu) {
-    menu->opcao = 0;
+    menu->opcaoSelecionada = 0;
     menu->totalOpcoes = 3;
+    strcpy(menu->nomeJogador, "");
     menu->tamanhoNome = 0;
-    menu->nomeJogador[0] = '\0';
 }
 
 void desenharMenuPrincipal(Menu *menu) {
-    const char *opcoes[3] = {
-        "Iniciar Jogo",
-        "Dificuldade",
-        "Pontuacao/Rank"
-    };
-    
+    ClearBackground(DARKGRAY);
+
+    DrawText("ARKANOID", SCREEN_WIDTH / 2 - 150, 100, 60, BLACK);
+
+    DrawText("ARKANOID", SCREEN_WIDTH / 2 - 150 + 3, 100 + 3, 60, RED);
+
+    const char *opcoes[] = {"Iniciar Jogo", "Dificuldades", "Ranking"};
+
     for (int i = 0; i < 3; i++) {
-        if (i == menu->opcao) {
-            printf(">>> %s <<<\n", opcoes[i]);
-        } else {
-            printf("    %s\n", opcoes[i]);
-        }
+        Color cor = (i == menu->opcaoSelecionada) ? (Color){255, 51, 51, 255} : WHITE;
+        DrawText(opcoes[i], SCREEN_WIDTH / 2 - 100, 250 + i * 50, 30, cor);
     }
+
+    DrawText("Use SETAS para navegar", SCREEN_WIDTH / 2 - 120, 450, 20, BLACK);
+    DrawText("ENTER para selecionar", SCREEN_WIDTH / 2 - 110, 475, 20, BLACK);
+    DrawText("ESC para sair", SCREEN_WIDTH / 2 - 70, 500, 20, BLACK);
 }
 
 void desenharMenuDificuldade(Menu *menu) {
-    const char *dificuldades[3] = {
-        "Facil",
-        "Medio",
-        "Dificil"
-    };
-    
-    printf("\n=== DIFICULDADE ===\n");
-    for (int i = 0; i < 3; i++) {
-        if (i == menu->opcao) {
-            printf(">>> %s <<<\n", dificuldades[i]);
-        } else {
-            printf("    %s\n", dificuldades[i]);
-        }
-    }
-}
+    ClearBackground(DARKGRAY);
 
-void desenharMenuRegistro(Menu *menu) {
-    printf("\n=== NOME DO JOGADOR ===\n");
-    printf("Digite seu nome: %s\n", menu->nomeJogador);
+    DrawText("SELECIONE A DIFICULDADE", SCREEN_WIDTH / 2 - 275, 100, 40, BLACK);
+
+    DrawText("SELECIONE A DIFICULDADE", SCREEN_WIDTH / 2 - 275 + 3, 100 + 3, 40, RED);
+
+    const char *opcoes[] = {"Facil", "Medio (+0.25x velocidade)", "Dificil (+0.5x velocidade)"};
+
+    for (int i = 0; i < 3; i++) {
+        Color cor = (i == menu->opcaoSelecionada) ? (Color){255, 51, 51, 255} : BLACK;
+        DrawText(opcoes[i], SCREEN_WIDTH / 2 - 150, 250 + i * 50, 25, cor);
+    }
+
+    DrawText("Use SETAS para navegar", SCREEN_WIDTH / 2 - 120, 450, 20, BLACK);
+    DrawText("ENTER para selecionar", SCREEN_WIDTH / 2 - 110, 475, 20, BLACK);
+    DrawText("ESC para voltar", SCREEN_WIDTH / 2 - 70, 500, 20, BLACK);
 }
 
 void desenharMenuGameOver(Menu *menu, int pontuacao, int tempo) {
-    printf("\n=== GAME OVER ===\n");
-    printf("Pontos: %d\n", pontuacao);
-    printf("Tempo: %d segundos\n", tempo);
-    printf("Pressione ENTER para voltar ao menu\n");
+    ClearBackground(DARKGRAY);
+
+    DrawText("GAME OVER", SCREEN_WIDTH / 2 - 150, 100, 60, BLACK);
+    DrawText("GAME OVER", SCREEN_WIDTH / 2 - 150 + 3, 100 + 3, 60, RED);
+
+    DrawText(TextFormat("Pontuacao: %d", pontuacao), SCREEN_WIDTH / 2 - 100, 200, 30, BLACK);
+    DrawText(TextFormat("Tempo: %d segundos", tempo), SCREEN_WIDTH / 2 - 120, 240, 30, BLACK);
+
+    const char *opcoes[] = {"Jogar Novamente", "Menu Principal"};
+
+    for (int i = 0; i < 2; i++) {
+        Color cor = (i == menu->opcaoSelecionada) ? (Color){255, 51, 51, 255} : BLACK;
+        DrawText(opcoes[i], SCREEN_WIDTH / 2 - 120, 320 + i * 50, 25, cor);
+    }
+
+    DrawText("Use SETAS para navegar", SCREEN_WIDTH / 2 - 120, 450, 20, BLACK);
+    DrawText("ENTER para selecionar", SCREEN_WIDTH / 2 - 110, 475, 20, BLACK);
 }
 
 void desenharMenuVitoria(Menu *menu, int pontuacao, int tempo) {
-    printf("\n=== VITORIA! ===\n");
-    printf("Pontos: %d\n", pontuacao);
-    printf("Tempo: %d segundos\n", tempo);
-    printf("Pressione ENTER para voltar ao menu\n");
+    ClearBackground(DARKGRAY);
+
+    DrawText("VITORIA!", SCREEN_WIDTH / 2 - 120, 100, 60, (Color){255, 200, 40, 255});
+    DrawText("Parabens! Voce completou todas as fases!", SCREEN_WIDTH / 2 - 250, 180, 20, BLACK);
+    DrawText("Parabens! Voce completou todas as fases!", SCREEN_WIDTH / 2 - 250 + 3, 180 + 3, 20, RED);
+
+    DrawText(TextFormat("Pontuacao Final: %d", pontuacao), SCREEN_WIDTH / 2 - 120, 230, 30, BLACK);
+    DrawText(TextFormat("Tempo Total: %d segundos", tempo), SCREEN_WIDTH / 2 - 140, 270, 30, BLACK);
+
+    const char *opcoes[] = {"Jogar Novamente", "Menu Principal"};
+
+    for (int i = 0; i < 2; i++) {
+        Color cor = (i == menu->opcaoSelecionada) ? (Color){255, 51, 51, 255} : BLACK;
+        DrawText(opcoes[i], SCREEN_WIDTH / 2 - 120, 340 + i * 50, 25, cor);
+    }
+
+    DrawText("Use SETAS para navegar", SCREEN_WIDTH / 2 - 120, 450, 20, BLACK);
+    DrawText("ENTER para selecionar", SCREEN_WIDTH / 2 - 110, 475, 20, BLACK);
 }
 
 void desenharMenuPausa(Menu *menu) {
-    printf("\n=== PAUSA ===\n");
-    printf("Pressione P para continuar\n");
+    DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGTH, Fade(BLACK, 0.7f));
+
+    DrawText("PAUSADO", SCREEN_WIDTH / 2 - 100, 150, 50, WHITE);
+
+    const char *opcoes[] = {"Continuar", "Menu Principal"};
+
+    for (int i = 0; i < 2; i++) {
+        Color cor = (i == menu->opcaoSelecionada) ? (Color){255, 51, 51, 255} : WHITE;
+        DrawText(opcoes[i], SCREEN_WIDTH / 2 - 80, 280 + i * 50, 25, cor);
+    }
+
+    DrawText("Use SETAS para navegar", SCREEN_WIDTH / 2 - 120, 400, 20, BLACK);
+    DrawText("ENTER para selecionar", SCREEN_WIDTH / 2 - 110, 425, 20, BLACK);
 }
 
 int atualizarMenu(Menu *menu) {
-    char entrada;
-    printf("\nUse W/S para navegar, ENTER para selecionar: ");
-    if (scanf("%c", &entrada) == 1) {
-        if (entrada == 'w' || entrada == 'W') {
-            menu->opcao = (menu->opcao - 1 + menu->totalOpcoes) % menu->totalOpcoes;
-            printf("\n");
-        } else if (entrada == 's' || entrada == 'S') {
-            menu->opcao = (menu->opcao + 1) % menu->totalOpcoes;
-            printf("\n");
-        } else if (entrada == '\n' || entrada == ' ') {
-            printf("\n");
-            return 1;
+    if (IsKeyPressed(KEY_UP)) {
+        menu->opcaoSelecionada--;
+        if (menu->opcaoSelecionada < 0) {
+            menu->opcaoSelecionada = menu->totalOpcoes - 1;
         }
     }
-    return 0;
+
+    if (IsKeyPressed(KEY_DOWN)) {
+        menu->opcaoSelecionada++;
+        if (menu->opcaoSelecionada >= menu->totalOpcoes) {
+            menu->opcaoSelecionada = 0;
+        }
+    }
+
+    if (IsKeyPressed(KEY_ENTER)) {
+        return menu->opcaoSelecionada;
+    }
+
+    return -1;
+}
+
+void desenharMenuRegistro(Menu *menu) {
+    ClearBackground(DARKGRAY);
+
+    int tituloLargura = MeasureText("DIGITE SEU NOME", 40);
+    DrawText("DIGITE SEU NOME", SCREEN_WIDTH / 2 - tituloLargura / 2, 100, 40, BLACK);
+    DrawText("DIGITE SEU NOME", SCREEN_WIDTH / 2 - tituloLargura / 2 + 3, 100 + 3, 40, RED);
+
+    DrawRectangle(SCREEN_WIDTH / 2 - 200, 200, 400, 60, (Color){153, 153, 153, 255});
+    DrawRectangleLines(SCREEN_WIDTH / 2 - 200, 200, 400, 60, WHITE);
+
+    if (menu->tamanhoNome > 0) {
+        int textoLargura = MeasureText(menu->nomeJogador, 30);
+        DrawText(menu->nomeJogador, SCREEN_WIDTH / 2 - textoLargura / 2, 215, 30, BLACK);
+    } else {
+        int placeholderLargura = MeasureText("Digite aqui...", 30);
+        DrawText("Digite aqui...", SCREEN_WIDTH / 2 - placeholderLargura / 2, 215, 30, BLACK);
+    }
+
+    DrawText("Digite seu nome (max 20 caracteres)", 
+             SCREEN_WIDTH / 2 - 180, 280, 20, BLACK);
+    DrawText("ENTER para confirmar", 
+             SCREEN_WIDTH / 2 - 120, 320, 20, BLACK);
+    DrawText("BACKSPACE para apagar", 
+             SCREEN_WIDTH / 2 - 135, 345, 20, BLACK);
+    DrawText("ESC para voltar", 
+             SCREEN_WIDTH / 2 - 75, 370, 20, BLACK);
 }
 
 bool atualizarMenuRegistro(Menu *menu) {
-    char entrada[50];
-    if (fgets(entrada, sizeof(entrada), stdin) != NULL) {
-        int len = strlen(entrada);
-        if (len > 0 && entrada[len - 1] == '\n') {
-            entrada[len - 1] = '\0';
+    int tecla = GetCharPressed();
+
+    while (tecla > 0) {
+        if ((tecla >= 32) && (tecla <= 125) && (menu->tamanhoNome < 20)) {
+            menu->nomeJogador[menu->tamanhoNome] = (char)tecla;
+            menu->nomeJogador[menu->tamanhoNome + 1] = '\0';
+            menu->tamanhoNome++;
         }
-        if (strlen(entrada) > 0) {
-            strncpy(menu->nomeJogador, entrada, sizeof(menu->nomeJogador) - 1);
-            menu->nomeJogador[sizeof(menu->nomeJogador) - 1] = '\0';
-            menu->tamanhoNome = strlen(menu->nomeJogador);
-            return true;
+        tecla = GetCharPressed();
+    }
+
+    if (IsKeyPressed(KEY_BACKSPACE)) {
+        if (menu->tamanhoNome > 0) {
+            menu->tamanhoNome--;
+            menu->nomeJogador[menu->tamanhoNome] = '\0';
         }
     }
+
+    if (IsKeyPressed(KEY_ENTER) && menu->tamanhoNome > 0) {
+        return true;
+    }
+
     return false;
 }
